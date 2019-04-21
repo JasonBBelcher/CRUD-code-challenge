7bd9b2f9c03461f05bbd3120b0e8d5a5cad1cb3e
Jason Belcher <jason.belcher@cumberlandgroupit.com>
2019-04-21 14:38:15 -0400

    add financial chart view to visualize AGRs

--- angular-targets-frontend/src/app/pages/view-financials/view-financials.component.ts
+++ angular-targets-frontend/src/app/pages/view-financials/view-financials.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { Observable } from 'rxjs';
+import { ApiService } from '../../api.service';
 
 @Component({
   selector: 'app-view-financials',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-financials.component.css']
 })
 export class ViewFinancialsComponent implements OnInit {
+  targets$: Observable<any[]>;
+  public pieChartOptions: ChartOptions = {
+    responsive: true
+  };
+  public pieChartLabels: Label[] = [];
+  public chartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
 
-  constructor() { }
+  public pieChartData: ChartDataSets[] = [{ data: [], label: '' }];
+
+  constructor(private apiService: ApiService) {}
 
   ngOnInit() {
-  }
+    this.apiService.getTargets();
+    this.targets$ = this.apiService.targets;
+    this.targets$.subscribe(targets => {
+      this.pieChartLabels = targets.map(target => {
+        return target.targetName;
+      });
+      const agrArr = [];
+      targets.map(target => {
+        return {
+          data: agrArr.push(target.agr.slice(0, -1)),
+          Label: 'Series A'
+        };
+      });
 
+      this.pieChartData[0].data = agrArr;
+    });
+  }
 }
